25abbd0787dd91988eb34ba61ef322608732c6c9
John Haugeland <stonecypher@gmail.com>
2014-05-28 00:30:50 -0700

    preprocessing

--- sql/preprocess.sql
+++ sql/preprocess.sql
@@ -1,1 +1,6 @@
-select 'TODO preprocess WHARGARBL' as 'Preprocessing data';
+select 'Fetching counts and means; can take several minutes' as 'Preprocessing data';
+
+update movies
+    set
+        rcount = (select count(*)    from ratings where movieid = movies.movieid),
+        mean   = (select avg(rating) from ratings where movieid = movies.movieid);
