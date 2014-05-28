b82bb06a7c880805159e701d7df2f7415a2b9c87
John Haugeland <stonecypher@gmail.com>
2014-05-27 20:08:48 -0700

    moar sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,7 +4,17 @@ create database if not exists nfp;
 use nfp;
 
 drop table if exists ratings;
-drop table if exists users;
 drop table if exists movies;
 
-create table movies
+create table movies(
+	mid   smallint primary key not null, 
+	mname varchar              not null, 
+	myear smallint             not null
+);
+
+create table ratings(
+	userid  mediumint not null,
+	movieid smallint  not null,
+	name    varchar   not null,
+	rdate   smallint  not null
+);
