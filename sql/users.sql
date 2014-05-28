7b15ea2182c11736bbffa95edda2fad03468b405
John Haugeland <stonecypher@gmail.com>
2014-05-28 00:51:30 -0700

    user stats

--- sql/users.sql
+++ sql/users.sql
@@ -1,3 +1,9 @@
-SELECT 'All statistical, all the time (can take several minutes)' as 'Generating user data';
+SELECT 'All statistical, all the time (three steps, can take several minutes)' as 'Generating user data';
 
-insert into users(userid, rcount, mean) select userid, count(*), avg(rating) from ratings group by userid;
+insert into users(userid, rcount, mean) select userid, count(*), avg(rating) from ratings where userid >=       1 and userid < 1000000 group by userid;
+insert into users(userid, rcount, mean) select userid, count(*), avg(rating) from ratings where userid >= 1000000 and userid < 2000000 group by userid;
+insert into users(userid, rcount, mean) select userid, count(*), avg(rating) from ratings where userid >= 2000000 and userid < 3000000 group by userid;
+
+alter table users add index(userid);
+alter table users add index(rcount);
+alter table users add index(mean);
